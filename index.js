d09933fbb408e5fd026c1e90dd16cf79fbfe3965
m49774686-hash <m49774686@gmail.com>
2026-07-13 23:47:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 require("dotenv").config();
 
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB Error:", err);
+  }
+}
+
+connectDB();
 const TelegramBot = require("node-telegram-bot-api");
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, {
